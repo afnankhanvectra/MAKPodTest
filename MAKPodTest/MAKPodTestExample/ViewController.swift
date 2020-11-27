2891371c571e3d6f1db1c591cc30270407d27be0
afnan khan <afnan.khan@irsavideo.com>
2020-11-27 12:12:09 +0500

    First test

---
+++ MAKPodTest/MAKPodTestExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAKPodTestExample
+//
+//  Created by afnan khan on 27/11/2020.
+//  Copyright © 2020 afnan khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
