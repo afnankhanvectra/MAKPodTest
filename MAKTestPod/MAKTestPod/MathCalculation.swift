daae118679854614c3519d6ec75973effcea00f2
afnan khan <afnan.khan@irsavideo.com>
2020-12-02 01:31:49 +0500

    uplad new peoject

---
+++ MAKTestPod/MAKTestPod/MathCalculation.swift
@@ -0,0 +1,28 @@
+//
+//  MathCalculation.swift
+//  MAKTestPod
+//
+//  Created by afnan khan on 02/12/2020.
+//
+
+import Foundation
+
+
+public  class MathCalculation {
+
+    let name = "MathCalculation"
+    
+    public init() {
+        
+    }
+
+    
+    open func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    open func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
---
+++ MAKTestPod/MAKTestPodExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MAKTestPodExample
+//
+//  Created by afnan khan on 02/12/2020.
+//
+
+import UIKit
+import MAKTestPod
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let math = MathCalculation.init()
+                
+                let number  = math.add(a:12 , b:20)
+                print("number is \(number)")
+        
+    }
+
+
+}
+
